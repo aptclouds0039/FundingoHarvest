7ae873b117b9faed2937442b8deff81db80a891e
Nishant Redu <nishantr@aptclouds.com>
2024-02-21 16:31:41 +0530

    Minor Updates

--- force-app/main/default/lwc/spExplorerRecord/spExplorerRecord.js
+++ force-app/main/default/lwc/spExplorerRecord/spExplorerRecord.js
@@ -1,133 +1,171 @@
 import { LightningElement, wire, api  } from 'lwc';
 import { getRecord, getFieldValue, updateRecord } from "lightning/uiRecordApi";
+
 import getToken from '@salesforce/apex/MicrosoftAuthentication.getToken';
+import getOppFolders from '@salesforce/apex/SharePointExplorerController.getOppFolders';
 import SHAREPOINT_FOLDER_ID from '@salesforce/schema/Opportunity.Sharepoint_Folder_Id__c';
 import OPP_NAME from '@salesforce/schema/Opportunity.Name';
 import ID_FIELD from "@salesforce/schema/Opportunity.Id";
-
+const ERROR_MSG     = 'ERROR';
+const METHOD_PUT    = 'PUT';
+const METHOD_POST   = 'POST';
+const METHOD_GET    = 'GET';
+const BEARER_STRING = 'Bearer ';
+const FILE_SELECTOR = '@microsoft.graph.downloadUrl';
+const ROOT_NAME     = 'Shared Documents';
 
 export default class SpExplorerRecord extends LightningElement {
     @api recordId
     someData = [];
+    driveId;
+    graphUrl;
     accessToken;
-
-    parentDirectory;
+    isRoot = true;
     currentFolderId;
     hasFolderId = false;
     folderId;
+    folderHeirarchy = [];
+    showFileUploadModal = false;
+    datatableColumns = [
+      {label: 'Name', type:'customName', typeAttributes: {isFolder: {fieldName: 'isFolder'}, folderName: {fieldName: 'name'}, fileId:{fieldName:'id'}, eTag: {fieldName: 'eTag'}}},
+      {label: 'Created By', fieldName:'createdBy'},
+      {label: 'Created DateTime', fieldName:'createdDateTime'},
+      {label: 'Actions', type:'customActionNew',
+          typeAttributes:{recId:{fieldName:'name'}, fileId:{fieldName:'id'}, eTag: {fieldName: 'eTag'}}
+      }
+  ]
+
+  filesToUpload = [];
+
     @wire(getRecord, {recordId:"$recordId", fields: [SHAREPOINT_FOLDER_ID, OPP_NAME]})
     getFolderId({error, data}){
         if(data){
             this.folderId = getFieldValue(data, SHAREPOINT_FOLDER_ID);
             this.oppName = getFieldValue(data, OPP_NAME);
             console.log('Folder Id')
-            this.getAccessToken(this.folderId);
+            this.getAccessToken();
             if(this.folderId != null){
                 this.currentFolderId = this.folderId;
                 this.hasFolderId = true;
-                
+                this.folderHeirarchy.push(
+                  {
+                    id: this.folderId,
+                    name: this.oppName,
+                  }
+                )
             }
             
         }
+    }
+
+    getOpportunityFolder(){
+      getOppFolders({oppId: this.recordId})
+      .then(res => {
+        console.log(JSON.stringify(res));
+        this.someData = res;
+        this.showLoadingSpinner = false;
+      })
+      .catch(error => {
+        console.log('Error ' + error);
+        this.showLoadingSpinner = false;
+        // Error loggin here
+      })
     }
 
     get showBackButton(){
         return this.folderId == this.currentFolderId;
     }
 
-    datatableColumns = [
-        {label: 'Name', type:'customName', typeAttributes: {isFolder: {fieldName: 'isFolder'}, folderName: {fieldName: 'name'}, fileId:{fieldName:'id'}, eTag: {fieldName: 'eTag'}}},
-        {label: 'Created By', fieldName:'createdBy'},
-        {label: 'Created DateTime', fieldName:'createdDateTime'},
-        {label: 'Actions', type:'customActionNew',
-            typeAttributes:{recId:{fieldName:'name'}, fileId:{fieldName:'id'}, eTag: {fieldName: 'eTag'}}
-        }
-    ]
-
-    filesToUpload = [];
+    
     connectedCallback(){
-        //this.getAccessToken();
-        //this.processData();
+        this.getOpportunityFolder();
     }
     
     handleFileClick(event){
+      this.showLoadingSpinner = true;
         console.log(event.detail.fileFolderObj.folderName);
         var isFolder = event.detail.fileFolderObj.folderType;
         var folderId = event.detail.fileFolderObj.fileId;
-        console.log(event.detail.fileFolderObj.fileId);
-        console.log(event.detail.fileFolderObj.eTag);
-        console.log(event.detail.fileFolderObj.folderType);
+        var folderName = event.detail.fileFolderObj.folderName;
         if(isFolder){
+          this.isRoot = false;
+          this.folderHeirarchy.push({
+            id: folderId,
+            name: folderName
+          })
           this.getSpecificFolderData(folderId);
         }else{
           this.downloadFile(folderId);
         }
         
     }
-    getAccessToken(folderId){
+
+    handleBackClick(event){
+      this.showLoadingSpinner = true;
+      console.log('Popping ');
+      this.folderHeirarchy.pop();
+      this.requestParentFolder(this.currentFolderId);
+    }
+
+    handleBreadCrumbClick(event){
+      this.showLoadingSpinner = true;
+      var lengthToKeep = Number(event.target.dataset.key) + 1;
+      var folderId = event.target.dataset.id;
+      console.log('Folder Id ' + folderId);
+      this.folderHeirarchy.length = lengthToKeep;
+      this.getSpecificFolderData(folderId);
+    }
+
+
+    getAccessToken(){
       getToken()
-      .then(token => {
-        console.log('Token ' + token);
-        this.accessToken = token;
-        if(folderId != null){
-            this.getSpecificFolderData(folderId);
-        }
-        
+      .then(data => {
+        console.log('Data ' + JSON.stringify(data));
+        this.driveId = data.driveId;
+        this.graphUrl = data.graphURL;
+        this.accessToken = data.access_token
+      })
+      .catch(error => {
+        this.hasErrors = true;
+        this.showLoadingSpinner = false;
       })
     }
 
-    handleBackClick(event){
-      
-      this.requestParentFolder(this.currentFolderId);
-      
+    openFileUploadModal(){
+      this.showFileUploadModal = true;
     }
-    getFolderData(token){
-      var url = 'https://graph.microsoft.com/v1.0/drives/b%21CVdKX821UEuzn-l9uZHsxjwMwyQd-g9CsMkaI8vhpxP39bNWl7NLQa626medTHBO/root/children';
-        var headers = {
-          'Authorization': 'Bearer '+ token
-        };
 
-        fetch(url, {
-          method: 'GET',
-          headers: headers
-        })
-        .then(response => {
-          if (!response.ok) {
-            return 'ERROR';
-          }
-          return response.json();
-        })
-        .then(data => {
-          if(data == 'ERROR'){
-            return;
-          }
-          this.processFolderData(data);
-          
-        })
-        .catch(error => {
-          console.error('Fetch Error:', error);
-        });
+    closeFileUploadModal(){
+      this.showFileUploadModal = false;
+      this.filesToUpload = [];
+      this.addedFilesNames = [];
     }
 
     getSpecificFolderData(folderId){
+      console.log('Getting Specific Folder Data');
       this.currentFolderId = folderId;
-      var url = 'https://graph.microsoft.com/v1.0/drives/b%21CVdKX821UEuzn-l9uZHsxjwMwyQd-g9CsMkaI8vhpxP39bNWl7NLQa626medTHBO/items/' + folderId +'/children';
+      if(this.currentFolderId == this.folderId){
+        this.getOpportunityFolder();
+        return;
+      }
+      var url = this.graphUrl + this.driveId + '/items/' + folderId +'/children';
       var headers = {
-        'Authorization': 'Bearer '+ this.accessToken
+        Authorization: BEARER_STRING+ this.accessToken
       };
 
       fetch(url, {
-        method: 'GET',
+        method: METHOD_GET,
         headers: headers
       })
       .then(response => {
         if (!response.ok) {
-          return 'ERROR';
+          return ERROR_MSG;
         }
         return response.json();
       })
       .then(data => {
-        if(data == 'ERROR'){
+        if(data == ERROR_MSG){
+          this.showLoadingSpinner = false;
           return;
         }
         this.processFolderData(data);
@@ -135,6 +173,7 @@ export default class SpExplorerRecord extends LightningElement {
       })
       .catch(error => {
         console.error('Fetch Error:', error);
+        this.showLoadingSpinner = false;
       });
     }
 
@@ -150,12 +189,12 @@ export default class SpExplorerRecord extends LightningElement {
         dataObj.isFolder = file.folder == undefined || file.folder==null ? false: true;
         dataObj.id = file.id;
         dataObj.eTag = file.eTag;
-        this.parentDirectory = file.parentReference.id;
         finalData.push(dataObj);
         
       })
       
       this.someData = finalData;
+      this.showLoadingSpinner = false;
     }
 
     downloadFile(fileId){
@@ -189,35 +228,39 @@ export default class SpExplorerRecord extends LightningElement {
 
     requestParentFolder(currentId){
       console.log('Cur Id ' + currentId);
-      var url = 'https://graph.microsoft.com/v1.0/drives/b%21CVdKX821UEuzn-l9uZHsxjwMwyQd-g9CsMkaI8vhpxP39bNWl7NLQa626medTHBO/items/'+currentId;
+      var url = this.graphUrl + this.driveId + '/items/' +currentId;
       var headers = {
-        'Authorization': 'Bearer '+ this.accessToken
+        Authorization: BEARER_STRING+ this.accessToken
       };
 
       fetch(url, {
-        method: 'GET',
+        method: METHOD_GET,
         headers: headers
       })
       .then(response => {
         if (!response.ok) {
           console.log('First Check failed');
+          this.showLoadingSpinner = false;
           return response.json();
         }
         return response.json();
       })
       .then(data => {
+        this.showLoadingSpinner = false;
         if(data == 'ERROR'){
           console.log('Second Check failed');
           return;
         }
         console.log('PRID ', data.parentReference.id);
         if(data.parentReference.id != undefined){
-
           this.getSpecificFolderData(data.parentReference.id);
+        }else{
+          this.isRoot = true;
         }
       })
       .catch(error => {
         console.error('Fetch Error:', error);
+        this.showLoadingSpinner = false;
       });
     }
 
@@ -231,36 +274,45 @@ export default class SpExplorerRecord extends LightningElement {
             continue;
         }
         var file = files[key];
+        this.addedFilesNames = [...this.addedFilesNames, file.name];
+        console.log('Added Files' + this.addedFilesNames);
         this.filesToUpload.push(file);
         console.log('FIels ' , this.filesToUpload);
       }
     }
 
     uploadFiles(event){
+      this.showFileUploadModal = false;
+      this.showLoadingSpinner = true;
       console.log('FIels ' , this.filesToUpload);
+      const fetchPromises = [];
       this.filesToUpload.forEach(file => {
         var  formData = new FormData();
         formData.append('file', file);
-        var url = 'https://graph.microsoft.com/v1.0/drives/b!CVdKX821UEuzn-l9uZHsxjwMwyQd-g9CsMkaI8vhpxP39bNWl7NLQa626medTHBO/items/' + this.currentFolderId + ':/' + file.name + ':/content';
-        fetch(url, {
-          method: 'PUT',
-            headers: {
-                'Authorization': 'Bearer ' + this.accessToken,
-                // Replace YOUR_ACCESS_TOKEN with the actual access token
-                'Content-Type': file.type,
-            },
-            body: formData,
-        })
-        .then(res => {
-          return res.json();
-        })
-        .then(data => {
-          console.log('Data ' + data);
-          this.getSpecificFolderData(this.currentFolderId);
-        })
+        var url = this.graphUrl + this.driveId + '/items/' + this.currentFolderId + ':/' + file.name + ':/content';
+        fetchPromises.push(
+          fetch(url, {
+            method: METHOD_PUT,
+              headers: {
+                  Authorization: BEARER_STRING + this.accessToken,
+                  'Content-Type': file.type,
+              },
+              body: formData,
+          })
+          .then(res => {
+            return res.json();
+          })
+          .then(data => {
+            console.log('Data ' + data);
+          })
+        );
+      });
+      Promise.all(fetchPromises)
+      .then(res => {
+        this.filesToUpload = [];
+        this.addedFilesNames = [];
+        this.getSpecificFolderData(this.currentFolderId);
       })
-      this.filesToUpload = [];
-      
       
     }
 
